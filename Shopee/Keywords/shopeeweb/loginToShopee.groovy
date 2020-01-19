72cdb9de6867b80260095dc1cda547978a16d555
khasogi27 <khasogi27@live.com>
2020-01-19 17:17:13 +0700

    add files

---
+++ Shopee/Keywords/shopeeweb/loginToShopee.groovy
@@ -0,0 +1,46 @@
+package shopeeweb
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import internal.GlobalVariable
+
+public class loginToShopee {
+	def static void login(){
+		def popup = findTestObject('login/popup-shopee')
+		def Login = findTestObject('Login/login-btn')
+		def Username = findTestObject('Login/Username-input')
+		def Password = findTestObject('Login/Password-input')
+		def submit = findTestObject('Login/login-btn2')
+
+		WebUI.openBrowser('')
+		WebUI.maximizeWindow()
+		WebUI.navigateToUrl('shopee.co.id')
+		WebUI.delay(2)
+		WebUI.click(popup)
+		WebUI.delay(2)
+		WebUI.click(Login)
+		WebUI.delay(2)
+		WebUI.setText(Username, 'khasogi27@gmail.com')
+		WebUI.delay(2)
+		WebUI.setText(Password, 'myloveallah')
+		WebUI.delay(2)
+		WebUI.click(submit)
+		WebUI.delay(2)
+	}
+}
---
+++ Shopee/Object Repository/Login/popup-shopee.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>popup-shopee</name>
+   <tag></tag>
+   <elementGuidId>ee9f6164-730d-4e81-a212-149d7577f3e9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/div[1]/*</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
